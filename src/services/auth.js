10c8c0b2eec20427b2f02286135c1cbc2cda9a06
Allison Ause <allison.ause@gmail.com>
2022-10-05 14:25:05 -0700

    added BrowserRouter and wrapped App. Created userContext for providing currentUser. Created user Service to sign-in, sign-up users in the supabase auth table

---
+++ src/services/auth.js
@@ -0,0 +1,23 @@
+import { client, checkError } from './client';
+
+export function getUser() {
+  return client.auth.currentUser;
+}
+
+export async function authUser(email, password, type) {
+  let response;
+  if (type === 'sign-up') {
+    try {
+      response = await client.auth.signUp({ email, password });
+    } catch (e) {
+      return checkError(e.message);
+    }
+  } else {
+    try {
+      response = await client.auth.signIn({ email, password });
+    } catch (e) {
+      return checkError(e.message);
+    }
+  }
+  return response.user;
+}
