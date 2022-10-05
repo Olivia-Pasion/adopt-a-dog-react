3a72f1e3d3bb1d5ff9639090073275c9f2f9fcc1
Allison Ause <allison.ause@gmail.com>
2022-10-05 14:32:56 -0700

    began to build out dom for rendering of Auth component. Added local state of type, email and password to deal with Auth service needs

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,3 +1,25 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Auth() {
-  return <div>Auth Page</div>;
+  const { type } = useParams();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // NavLink instead of h1? Highlight if active
+
+  // redirect if user needs to sign up instead of sign in
+
+  // create onClick handler for auth
+
+  return (
+    <div>
+      <div>
+        <h1>{type}</h1>
+        <input value={email} name="email" />
+        <input type="password" value={password} name="password" />
+        <button></button>
+      </div>
+    </div>
+  );
 }
