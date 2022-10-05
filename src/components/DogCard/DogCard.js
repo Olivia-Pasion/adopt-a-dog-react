9646376ba2f7cc4051e3a13e14ecc2140e550744
olivia pasion <oap9820@gmail.com>
2022-10-05 15:48:26 -0700

    DogCard component started

---
+++ src/components/DogCard/DogCard.js
@@ -0,0 +1,7 @@
+
+
+export default function DogCard({ name, breed, bio, image }) {
+  return (
+    <div>DogCard</div>
+  )
+}
--- src/components/Dogs/Dogs.js
+++ src/components/Dogs/Dogs.js
@@ -1,6 +1,7 @@
-
+import { useDogs } from '../../hooks/useDogs';
 
 export default function Dogs() {
+  
   return (
     <div>Dogs</div>
   );
