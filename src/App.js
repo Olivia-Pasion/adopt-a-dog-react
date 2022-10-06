0ad0488ebd3f9b675d5e317cb0be79fa7421023b
Aphenphos <MaxwillWinters@gmail.com>
2022-10-06 18:12:54 -0400

    edit button properly redirects to update page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
 import Dogs from './components/Dogs/Dogs';
 import NewDog from './components/NewDog/NewDog';
+import UpdateDog from './components/UpdateDog/UpdateDog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/auth/:type" component={Auth} />
         <Route exact path="/" component={Dogs} />
         <Route path="/newdog" component={NewDog} />
+        <Route path="/updatedog/:id" component={UpdateDog} />
       </Switch>
     </div>
   );
--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -1,12 +1,19 @@
+import { useContext } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { UserContext } from '../../context/userContext';
 import './DogCard.css';
-export default function DogCard({ name, breed, bio, image }) {
+export default function DogCard({ id, name, breed, bio, image, user_id }) {
+  const { user } = useContext(UserContext);
   //add delete button and edit button to return
+  const owner = user.id === user_id;
+
   return (
     <div className="dog-card">
       <img src={image}></img>
       <span>{`Hi! My name is ${name}`}</span>
       <span>{breed}</span>
       <p>{bio}</p>
+      {owner && <Link to={`/updatedog/${id}`}>Edit</Link>}
     </div>
   );
 }
