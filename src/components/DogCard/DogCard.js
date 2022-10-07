b9203e0f179db80d69f688fa7f66938cf770161a
Aphenphos <MaxwillWinters@gmail.com>
2022-10-06 20:38:13 -0400

    can delete properly

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -1,11 +1,16 @@
 import { useContext } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
+import { deleteDog } from '../../services/dogs';
 import './DogCard.css';
 export default function DogCard({ id, name, breed, bio, image, user_id }) {
   const { user } = useContext(UserContext);
   //add delete button and edit button to return
   const owner = user.id === user_id;
+
+  const handleAdopt = async () => {
+    await deleteDog({ id });
+  };
 
   return (
     <div className="dog-card">
@@ -13,7 +18,12 @@ export default function DogCard({ id, name, breed, bio, image, user_id }) {
       <span>{`Hi! My name is ${name}`}</span>
       <span>{breed}</span>
       <p>{bio}</p>
-      {owner && <Link to={`/updatedog/${id}`}>Edit</Link>}
+      {owner && (
+        <>
+          <Link to={`/updatedog/${id}`}>Edit</Link>
+          <button onClick={handleAdopt}>🏘️</button>
+        </>
+      )}
     </div>
   );
 }
--- src/services/dogs.js
+++ src/services/dogs.js
@@ -19,6 +19,14 @@ export async function addNewDog(NewDog) {
 }
 
 export async function updateDog({ id, name, breed, bio, image }) {
-  const response = await client.from('adopt-a-dog').update({ name, breed, bio, image }).match({ id });
+  const response = await client
+    .from('adopt-a-dog')
+    .update({ name, breed, bio, image })
+    .match({ id });
+  console.log(response);
+}
+
+export async function deleteDog({ id }) {
+  const response = await client.from('adopt-a-dog').delete().match({ id }).single();
   console.log(response);
 }
