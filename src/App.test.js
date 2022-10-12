ca045b4cd6e738140c17c46cb03d79e1b293e719
Allison Ause <allison.ause@gmail.com>
2022-10-12 12:23:37 -0700

    passing test for successfully updating dog

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, getByRole, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './context/userContext';
@@ -79,7 +80,62 @@ test('user can view all dogs and interact with their own', async () => {
   await screen.findByText(/Duncan/i);
   await screen.findByText(/Marv/i);
 
-  //user sees edit & delete buttons
-  await screen.findByText(/Modify Info/i);
+  //user can see edit & delete buttons for their own dog
   await screen.findByLabelText(/adopt-button/i);
+  const modifyButton = await screen.findByText(/Modify Info/i);
+  fireEvent.click(modifyButton);
+
+  //this confirms that user has been redirected after clickHandler has been fired
+  const updateHeader = await screen.findByText(/Update Dog/i);
+  expect(updateHeader).toBeInTheDocument();
 });
+
+test('user can update their own dog', async () => {
+  //user is already logged in
+  authFns.getUser.mockReturnValue(testUser);
+  dogFns.getDogDetail.mockReturnValue(testDogsList[0]);
+  dogFns.getDogs.mockReturnValue(testDogsList);
+
+  render(
+    <UserProvider>
+      <MemoryRouter initialEntries={['/updatedog/1']}>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
+
+  //dog detail loads with established information
+  const dogNameInput = await screen.findByLabelText(/Name:/i);
+  expect(dogNameInput.value).toBe('Duncan');
+
+  //inputs change value
+  const nameInput = await screen.findByLabelText(/Name:/i);
+  await act(async () => {
+    fireEvent.change(nameInput, { target: { value: 'Quimby' } });
+    expect(nameInput.value).toBe('Quimby');
+  });
+
+  const breedInput = await screen.findByLabelText(/Breed:/i);
+  await act(async () => {
+    fireEvent.change(breedInput, { target: { value: 'Irish Wolfhound' } });
+    expect(breedInput.value).toBe('Irish Wolfhound');
+  });
+
+  const bioInput = await screen.findByLabelText(/Bio:/i);
+  await act(async () => {
+    fireEvent.change(bioInput, { target: { value: 'Lanky AF' } });
+    expect(bioInput.value).toBe('Lanky AF');
+  });
+
+  const imageURLInput = await screen.findByLabelText(/Image URL:/i);
+  await act(async () => {
+    fireEvent.change(imageURLInput, { target: { value: 'http://dog-url.com' } });
+    expect(imageURLInput.value).toBe('http://dog-url.com');
+  });
+
+  //button event fires
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
+});
+
+//queryByRole to expect().not.toBeInTheDocument();
